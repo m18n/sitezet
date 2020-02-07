c1c98860894704ab27e1d4aaa3c16bcd6245c5b1
m18n <37105504+m18n@users.noreply.github.com>
2020-02-07 21:47:12 +0200

    калькулятор

--- zet3+1R3026.php
+++ zet3+1R3026.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/zet3+1.css">
+    
     <title>zet 3+1 R30 26</title>
   </head>
   <body>
@@ -34,11 +35,129 @@
     </div>
   </nav>
   <div class="wrapper">
-    <div class="photo">
-      <img src="img/lamp/zet 3+1 R30 26/IMG_7058.JPG">
+    <div class="center">
+      <div class="photo">
+        <img src="img/lamp/zet 3+1 R30 26/IMG_7058.JPG">
+      </div>
+      <!-- kalulator-->
+      <div class="kalulator">
+        <div class="back">
+          <div class="katigrao">
+            <div class="option1">
+              <div class="texetoption1">
+           <a id="led">LED Центр</a>
+           </div>
+             <div class="optiontext">
+             <select id="selectBox" onchange="changeFunc();">
+              <option value="0">XHP-50.2 (3000K)</option>
+              <option  value="0">XHP-50.2 (4000K)</option>
+              <option  value="0">XHP-50.2 (5000K)</option>
+              <option  value="10">XHP-70.2 (3000K)</option>
+              <option  value="10">XHP-70.2 (4000K)</option>
+              <option  value="10">XHP-70.2 (5000K)</option>
+             </select>
+             </div>
+             </div>
+             <div class="option1">
+                <div class="texetoption1">
+             <a>LED Периферия</a>
+            </div>
+             <div class="optiontext">
+             <select id="selectBox1" onchange="changeFunc();">
+              <option  value="0">XPG (3000K)</option>
+              <option  value="0">XPG (4000K)</option>
+              <option  value="0">XPG (5000K)</option>
+              <option  value="0">XPG (6000K)</option>
+             </select>
+             </div>
+            </div>
+            <div class="option1">
+                <div class="texetoption1">
+             <a>Магнитная кнопка</a>
+            </div>
+            <div class="optiontext">
+              <select id="selectBox2" onchange="changeFunc();">
+              <option  value="0">Нажимная</option>
+              <option  value="10">Бегунок</option>
+              
+             </select>
+             </div>
+            </div>
+            <div class="option1">
+                <div class="texetoption1">
+             <a >Корпус с кассетой</a>
+            </div>
+             <div class="optiontext">
+              <select id="selectBox3" onchange="changeFunc();">
+              <option  value="0">Под 4 АКБ</option>
+              <option  value="20">Под 8 АКБ</option>
+              
+             </select>
+            </div>
+          </div>
+            <div class="option1">
+                <div class="texetoption1">
+             <a>АКБ</a>
+            </div>
+             <div class="optiontext">
+              <select id="selectBox4" onchange="changeFunc();">
+              <option  value="0">Без АКБ</option>
+              <option  value="15">4 АКБ</option>
+              <option  value="30">8 АКБ</option>
+             </select>
+             </div>
+            </div>
+            <div class="option1">
+                <div class="texetoption1">
+             <a>Блок питания 16.8v</a>
+            </div>
+            <div class="optiontext">
+              <select id="selectBox5" onchange="changeFunc();">
+              <option  value="0">Без БП</option>
+              <option  value="10">С БП</option>
+              
+             </select>
+            </div>
+          </div>
+            <div class="option1">
+                <div class="texetoption1">
+             <a>Балансировочное ЗУ</a>
+            </div>
+             <div class="optiontext">
+              <select id="selectBox6" onchange="changeFunc();">
+              <option value="0">Без БЗУ</option>
+              <option value="30">С БЗУ</option>
+              
+             </select>
+            </div>
+          </div>
+
+             <div class="option1">
+                <div class="texetoption1">
+             <a>Плата Универсальная Зарядная</a>
+            </div>
+            <div class="optiontext">
+              <select id="selectBox7" onchange="changeFunc();">
+              <option  value="0">Без ПЗУ</option>
+              <option  value="15">С ПЗУ</option>
+              
+             </select>
+            </div>
+          </div>
+
+          <div class="restext">
+             <a id="res">200$</a>
+          </div>
+
+            </div>
+          </div>
+                </div>
+
+      <!-- end kalulator-->
     </div>
   </div>
 
+    <script type="text/javascript" src="javasript/kal.js"></script>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
